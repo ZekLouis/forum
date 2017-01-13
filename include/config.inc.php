45675f10783ed28fdfedb2d4f6ffa9c3d162104c
Louis Gaume <Louis Gaume>
2017-01-13 09:49:59 +0100

    bd

--- include/config.inc.php
+++ include/config.inc.php
@@ -1,7 +1,7 @@
 <?php
     define('DBHOST', "db664868866.db.1and1.com");
     define('DBNAME', "db664868866");
-    define('DBUSER', "forum");
+    define('DBUSER', "dbo664868866");
     define('DBPASSWD', "CTHeD3jM2J0wHjde");
     define('ENV','dev');
 
