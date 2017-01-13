b153e68159eccb5ad1cd32495ea915c1bc6b8157
Louis Gaume <louis.gaume@etu.unilim.fr>
2017-01-13 15:27:21 +0100

    LOCATION

--- include/pages/randomPost.inc.php
+++ include/pages/randomPost.inc.php
@@ -3,5 +3,5 @@
     $PostManager = new PostManager($db);
     $id = $PostManager->RandId();
     //echo "<script>window.location.replace(\"index.php?page=6&post=".$id."\");</script>";
-    header('location: index.php?page=6&post='.$PostManager->RandId());
+    header('Location: index.php?page=6&post='.$PostManager->RandId());
 ?>
