4a3cf78a08eaa0b645622c9bde4be79111742349
Louis Gaume <louis.gaume@etu.unilim.fr>
2017-01-13 15:19:02 +0100

    conf

--- include/config.inc.php
+++ include/config.inc.php
@@ -1,12 +1,12 @@
 <?php
-    /*define('DBHOST', "db664868866.db.1and1.com");
+    define('DBHOST', "db664868866.db.1and1.com");
     define('DBNAME', "db664868866");
     define('DBUSER', "dbo664868866");
-    define('DBPASSWD', "CTHeD3jM2J0wHjde");*/
-    define('DBHOST', "louisgaume.ddns.net");
+    define('DBPASSWD', "CTHeD3jM2J0wHjde");
+    /*define('DBHOST', "louisgaume.ddns.net");
     define('DBNAME', "forum");
     define('DBUSER', "forum");
-    define('DBPASSWD', "CTHeD3jM2J0wHjde");
+    define('DBPASSWD', "CTHeD3jM2J0wHjde");*/
     define('ENV','dev');
 
 ?>
--- include/pages/randomPost.inc.php
+++ include/pages/randomPost.inc.php
@@ -2,6 +2,6 @@
     $db = new Mypdo();
     $PostManager = new PostManager($db);
     $id = $PostManager->RandId();
-    echo "<script>window.location.replace(\"index.php?page=6&post=".$id."\");</script>";
-    //header('location: index.php?page=6&post='.$PostManager->RandId());
+    //echo "<script>window.location.replace(\"index.php?page=6&post=".$id."\");</script>";
+    header('location: index.php?page=6&post='.$PostManager->RandId());
 ?>
